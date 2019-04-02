2eaf00b3a2a2083d9dc4a983ea898e4366ca3e1e
KarloKralj <karlokraljposao@gmail.com>
2019-04-02 15:28:00 +0200

    Added: Sidebar and changed links

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function openMenu(){
+    document.getElementById('side-menu').style.width = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeMenu(){
+    document.getElementById('side-menu').style.width = "0px";
+    //document.getElementById("main").style.marginLeft = "0px";
+}
+
+function index(){
+    window.open("./index.html", "_self");
+}
+
+function jelacic(){
+    window.open("./trg.html", "_self");
+}
+
+function tomislav(){
+    window.open("./tomek.html", "_self");
+}
+
+function zrinski(){
+    window.open("./zrinja.html", "_self");
+}
